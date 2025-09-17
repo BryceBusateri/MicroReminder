a40f9b6cf8f02d29cccfe6fb13f0c1406e0c6868
Bryce Busateri <busateribryce@gmail.com>
2025-09-17 16:32:56 -0500

    intial commit

--- src/main.go
+++ src/main.go
@@ -31,7 +31,7 @@ func setReminderHandler(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	reminders = append(reminders, rem)
 	mu.Unlock()
-//200 response
+	//200 responses
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Reminder set!"))
 }
